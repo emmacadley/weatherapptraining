76c4b7c4d945b04814591a8d888ac383fddbdf4b
emmacadley <emma.cadley@postoffice.co.uk>
2024-11-20 16:39:36 +0000

    Initial Commit

--- starter-files/src/App.tsx
+++ starter-files/src/App.tsx
@@ -1,7 +1,26 @@
-const App = () => {
+import Search from './components/Search'
+import Forecast from './components/Forecast'
+import useForecast from './components/hooks/useForecast'
+
+
+const App = (): JSX.Element => {
+  
+const {term, options, forecast, onInputChange, onOptionSelect, onSubmit,
+} = useForecast()
+
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      Hello world
+      {forecast ?( 
+        <Forecast data={forecast} />
+        ):(
+     <Search 
+        term={term} 
+        options={options} 
+        onInputChange={onInputChange} 
+        onOptionSelect={onOptionSelect} 
+        onSubmit={onSubmit}
+        />
+        )}
     </main>
   )
 }
